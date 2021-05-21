e007ed3c2dac449baecb382588659292e9ccd8e6
Ram Rachure <46816331+ramkrishna2k3@users.noreply.github.com>
2021-05-21 17:21:48 +0530

    Update createinstance.tf

--- createinstance.tf
+++ createinstance.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAUHKJO4XAMB6VYZUP"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 
 }
